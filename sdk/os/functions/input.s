cd3b9ea775c856b57caf04f536502d82fc1c483a
Codecrafter_404 <codecrafter@4o4.me>
2021-09-21 14:18:42 +0200

    Applied addresses.json

--- sdk/os/functions/input.s
+++ sdk/os/functions/input.s
@@ -1,5 +1,5 @@
 .include "os/functions/_util.inc"
 
-DEFINE_OS_FUNC Input_GetKeyState 0x8006FE58
-DEFINE_OS_FUNC Input_IsAnyKeyDown 0x8006FF00
-DEFINE_OS_FUNC GetInput 0x8007CE2C
+DEFINE_OS_FUNC Input_GetKeyState 0x8006fdc0
+DEFINE_OS_FUNC Input_IsAnyKeyDown 0x8007002c
+DEFINE_OS_FUNC GetInput 0x8007cf64
